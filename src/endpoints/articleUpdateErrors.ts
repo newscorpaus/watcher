c99d915908a2f25f7a477801278dfbd192ef7a63
nicholasf <faizn1@nca1031460.news.newslimited.local>
2018-11-09 08:45:43 +1100

    cleaning up

--- src/endpoints/articleUpdateErrors.ts
+++ src/endpoints/articleUpdateErrors.ts
@@ -3,29 +3,18 @@ import { db } from './../db';
 import { keys, filter } from 'ramda';
 
 const articleUpdateReport = (req: Request, res: Response, next: NextFunction) => {
-    console.log('a!!!!');
     const articleIds = keys(db);
-    console.log('b');
 
     const finder = (articleId: string) => {
         return db[articleId].valid == false;
     };
 
-    console.log(1);
-
     const incompleteUpdates = filter(finder, articleIds);
     const incompletes: any = [];
-
-    console.log(2);
 
     incompleteUpdates.forEach((articleId: any) => {
         incompletes.push(db[articleId]);
     });
-
-    console.log(3);
-
-    incompletes.push('test');
-    console.log('incompletes: ', incompletes);
 
     res.json(incompletes);
 };
